0ae6b8aa09b95dba87ff7f45824440f7484cde39
Igor <igglike@gmail.com>
2023-06-30 19:23:45 +0600

    Добавил стили нав панели

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,7 +4,7 @@ import Navbar from './Navbar'
 
 const Layout = () => {
   return (
-    <div>
+    <div className="grid grid-cols-4 grid-flow-row auto-rows-min gap-4 bg-gray-100 h-screen text-center text-slate-900">
       <Navbar />
       <Outlet />
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,26 @@
+import {Link, NavLink} from 'react-router-dom'
+
+
+const activeLinkClass = ({isActive}) => {
+  return isActive ? 'rounded-md bg-sky-300 p-1 text-gray-700' : 'p-1 text-slate-50'
+} 
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="w-full flex justify-center align-center h-16 col-span-4 bg-sky-800">
+      <ul className="flex items-center gap-6 list-none">
+        <li>
+          <NavLink to="/" className={activeLinkClass}>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/counter" className={activeLinkClass}>Counter</NavLink>
+        </li>
+        <li>
+          <NavLink to="/todos" className={activeLinkClass}>Todo List</NavLink>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
