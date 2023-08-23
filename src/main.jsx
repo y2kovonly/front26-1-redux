69f82ebeae8cb180e063dd75e3ec74b56adf9182
Igor <igglike@gmail.com>
2023-08-23 20:03:42 +0600

    Инициализировал стор, добавил редьюсер счетчика

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,45 @@
-import ReactDOM from 'react-dom/client'
-import {BrowserRouter} from 'react-router-dom'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { legacy_createStore as createStore } from "redux";
 
-import './index.css'
-import App from './App'
+import "./index.css";
+import App from "./App";
 
+const defaultState = {
+  counter: 10,
+  name: '',
+  age: 1
+};
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// редьюсер - reducer
+// action - команда, что нужно сделать с состоянием
+// action - это объект с обязательным ключом type
+// {type: 'INCREASE_COUNTER'}
+// {type: 'DECREASE_COUNTER'}
+const counterReducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case "INCREASE_COUNTER":
+      // state = {counter: 11}
+      // return {counter: 11}
+      return { ...state, counter: state.counter + action.payload };
+    case "DECREASE_COUNTER":
+      return state;
+    default:
+      return state;
+  }
+};
+
+// action creators
+export const increaseCounter = () => ({ type: "INCREASE_COUNTER" });
+export const decreaseCounter = () => ({ type: "DECREASE_COUNTER" });
+
+const store = createStore(counterReducer);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
--- src/pages/CounterPage.jsx
+++ src/pages/CounterPage.jsx
@@ -1,15 +1,30 @@
+import { useDispatch, useSelector } from "react-redux";
+import { increaseCounter } from "../main";
 
 const CounterPage = () => {
+  const counter = useSelector((state) => state.counter);
+
+  const dispatch = useDispatch();
+
+  const handleIncrease = () => {
+    dispatch({ type: "INCREASE_COUNTER", payload: 3 });
+  };
+
   return (
     <div className="w-full flex flex-col justify-center gap-3">
-      <span className='text-2xl col-span-full'>Счетчик</span>
+      <span className="text-2xl col-span-full">Счетчик</span>
       <div className="w-full flex justify-center items-center gap-4">
         <button className="w-8 bg-slate-300 px-2 py-1 rounded-md">-</button>
-        <span>0</span>
-        <button className="w-8 bg-slate-300 px-2 py-1 rounded-md">+</button>
+        <span>{counter}</span>
+        <button
+          className="w-8 bg-slate-300 px-2 py-1 rounded-md"
+          onClick={handleIncrease}
+        >
+          +
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CounterPage
+export default CounterPage;
